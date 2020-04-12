22b7f94b9db02ca0c5c7635bc5cf34cceb179aaf
TakuroSugahara <k5690033@gmail.com>
2020-04-12 20:02:22 +0900

    feat: プラットフォームのロゴ

--- enum/platform.enum.ts
+++ enum/platform.enum.ts
@@ -2,3 +2,8 @@ export enum PlatformEnum {
   RAKUTEN = '楽天',
   AMAZON = 'Amazon'
 }
+
+export enum PlatformLogoEnum {
+  RAKUTEN = 'https://zaikosokuho.com/image/icon_rakuten.png',
+  AMAZON = 'https://zaikosokuho.com/image/icon_amazon.png'
+}
