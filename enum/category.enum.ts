82ba5ae4aa8eb4d986b1aa25e695f57becc09788
TakuroSugahara <k5690033@gmail.com>
2020-04-13 21:23:26 +0900

    feat: カテゴリーにアイコンを追加

--- enum/category.enum.ts
+++ enum/category.enum.ts
@@ -1,13 +1,32 @@
 export enum CategoryEnum {
   MASK = 'マスク',
   ALCOHOL_GEL = 'アルコールジェル',
-  ALCOHOL = 'アルコール',
-  GAME = 'ゲーム'
+  ALCOHOL_SPRAY = 'アルコールスプレー',
+  ALCOHOL_TISSUE = 'アルコールティッシュ'
+}
+
+enum CategoryIconEnum {
+  MASK = '/icon/covid.png',
+  ALCOHOL_GEL = '/icon/shampoo.png',
+  ALCOHOL_SPRAY = '/icon/clean.png',
+  ALCOHOL_TISSUE = '/icon/healthcare-and-medical.png'
 }
 
 export const CATEGORIES = [
-  CategoryEnum.MASK,
-  CategoryEnum.ALCOHOL,
-  CategoryEnum.ALCOHOL_GEL,
-  CategoryEnum.GAME
+  {
+    name: CategoryEnum.MASK,
+    icon: CategoryIconEnum.MASK
+  },
+  {
+    name: CategoryEnum.ALCOHOL_GEL,
+    icon: CategoryIconEnum.ALCOHOL_GEL
+  },
+  {
+    name: CategoryEnum.ALCOHOL_SPRAY,
+    icon: CategoryIconEnum.ALCOHOL_SPRAY
+  },
+  {
+    name: CategoryEnum.ALCOHOL_TISSUE,
+    icon: CategoryIconEnum.ALCOHOL_TISSUE
+  }
 ]
